58b35b65a03bc14a9e5ff0e9a2f89688ae652db1
lslz627 <lslz627@gmail.com>
2023-12-09 02:45:26 +0800

    add debian11 yazi.sh

---
+++ debian11/yazi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://yazi-rs.github.io/docs/installation
+# 1:install nerd-fonts
+bash -c  "$(curl -fsSL https://raw.githubusercontent.com/officialrajdeepsingh/nerd-fonts-installer/main/install.sh)" 
+
+apt install ffmpegthumbnailer unar jq poppler-utils fd-find riggrep fzf zoxide -y
+
